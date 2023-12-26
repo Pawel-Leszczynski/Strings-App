d5f32ccc315283f4e7f8510d3542e41b7e58647f
Pawel Leszczynski <pawelcio1337@gmail.com>
2023-12-26 18:41:18 +0100

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,21 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black">
       <div className="flex flex-col gap-2 p-5 max-w-xs w-full bg-slate-800 text-gray-300 rounded-lg">
-        <div className="text-center my-4 ">
+        <div className="text-center my-4">
           <h1>Strings</h1>
         </div>
         <div>
           <Link
             href="/signin"
-            className="bg-slate-900 my-4 p-3 rounded-lg block"
+            className="bg-slate-900 my-3 p-3 rounded-lg block"
           >
             Sign In
           </Link>
         </div>
         <div>
           <Link
-            href="/signup"
-            className="bg-slate-900 my-4 p-3 rounded-lg block"
+            href="/singup"
+            className="bg-slate-900 my-3 p-3 rounded-lg block"
           >
             Sign Up
           </Link>
